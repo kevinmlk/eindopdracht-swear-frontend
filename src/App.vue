639a442a046091ec8aac40781eb0d06be97e0503
Kevin Malekera <r0900654@student.thomasmore.be>
2024-11-28 23:40:55 +0100

    feat: added nav component and video header on the app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script setup>
-
+  // Import navigation component
+  import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
-  
+  <!-- Start Container -->
+  <el-container>
+    <!-- Start Header -->
+    <el-header>
+      <!-- Start Navigation -->
+      <Navigation />
+      <!-- End Navigation -->
+
+      <!-- Start Header Video -->
+      <div class="header-video">
+        <video autoplay loop muted>
+          <source src="./assets/video-header-swear.mp4" type="video/mp4">
+        </video>
+      </div>
+      <!-- End Header Video -->
+    </el-header>
+    <!-- End Header -->
+
+    <!-- Start Main -->
+    <el-main>
+
+    </el-main>
+    <!-- End Main -->
+  </el-container>
 </template>
 
 <style scoped>
