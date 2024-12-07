f838aa6af0e66aade7f77f1cc74a24117c2166aa
Kevin Malekera <r0900654@student.thomasmore.be>
2024-12-07 16:40:53 +0100

    feat: added login form input fields

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,78 @@
 <script setup>
-  
+  import { reactive, ref } from 'vue';
+
+const ruleFormRef = ref(null);
+
+const ruleForm = reactive({
+  username: '',
+  password: '',
+});
+
+const submitForm = async (formEl) => {
+  if (!formEl) return;
+  formEl.validate(async (valid) => {
+    if (valid) {
+      try {
+        const response = await fetch('http://localhost:3000/api/v1/users/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(ruleForm),
+        });
+
+        console.log(ruleForm);
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log('Form submitted successfully:', data);
+        // Reset form after successful submission
+        resetForm(formEl);
+      } catch (error) {
+        console.error('Error submitting the form:', error);
+      }
+    } else {
+      console.log('Error submitting the form!');
+    }
+  });
+};
 </script>
 
 <template>
   <h1>Login</h1>
+  <h2>Please fill out the form below</h2>
+  <el-form
+    ref="ruleFormRef"
+    style="max-width: 600px"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="auto"
+    class="demo-ruleForm"
+  >
+    <!-- Username -->
+    <el-form-item label="Username" prop="username">
+      <el-input v-model="ruleForm.username" />
+    </el-form-item>
+  <!-- Password -->
+    <el-form-item label="Password" prop="pass">
+      <el-input v-model="ruleForm.password" type="password" autocomplete="off" />
+    </el-form-item>
+
+   <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)">
+        Submit
+      </el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <style lang="scss" scoped>
-
+  #form-fields {
+    display: flex;
+    gap: 48px;
+  }
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,47 +1,88 @@
 <script setup>
-  import { reactive, ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const ruleFormRef = ref(null);
 
-const validatePass = (rule, value, callback) => {
-  if (value === '') {
-    callback(new Error('Please input the password'));
-  } else {
-    if (ruleForm.checkPass !== '') {
-      if (!ruleFormRef.value) return;
-      ruleFormRef.value.validateField('checkPass');
-    }
-    callback();
-  }
-};
+// const checkFirstname = (rule, value, callback) => {
+//   if (value === '') {
+//     return callback(new Error('Please input a firstname'))
+//   } else {
+//     callback()
+//   }
+// }
 
-const validatePass2 = (rule, value, callback) => {
-  if (value === '') {
-    callback(new Error('Please input the password again'));
-  } else if (value !== ruleForm.pass) {
-    callback(new Error("Two inputs don't match!"));
-  } else {
-    callback();
-  }
-};
+// const validatePass = (rule, value, callback) => {
+//   if (value === '') {
+//     callback(new Error('Please input the password'));
+//   } else {
+//     if (ruleForm.checkPass !== '') {
+//       if (!ruleFormRef.value) return;
+//       ruleFormRef.value.validateField('checkPass');
+//     }
+//     callback();
+//   }
+// };
+
+// const validatePass2 = (rule, value, callback) => {
+//   if (value === '') {
+//     callback(new Error('Please input the password again'));
+//   } else if (value !== ruleForm.pass) {
+//     callback(new Error("Two inputs don't match!"));
+//   } else {
+//     callback();
+//   }
+// };
 
 const ruleForm = reactive({
-  pass: '',
-  checkPass: '',
+  firstname: '',
+  lastname: '',
+  email: '',
+  username: '',
+  password: '',
+  phone: '',
+  role: 'user',
+  address: {
+    street: '',
+    city: '',
+    postalCode: '',
+    country: ''
+  }
 });
 
-const rules = reactive({
-  pass: [{ validator: validatePass, trigger: 'blur' }],
-  checkPass: [{ validator: validatePass2, trigger: 'blur' }],
-});
+// const rules = reactive({
+//   checkFirstname: [{ validator: checkFirstname, trigger: 'blur' }],
+//   pass: [{ validator: validatePass, trigger: 'blur' }],
+//   checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+// });
 
-const submitForm = (formEl) => {
+const submitForm = async (formEl) => {
   if (!formEl) return;
-  formEl.validate((valid) => {
+  formEl.validate(async (valid) => {
     if (valid) {
-      console.log('submit!');
+      try {
+        const response = await fetch('http://localhost:3000/api/v1/users/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(ruleForm),
+        });
+
+        console.log(ruleForm);
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log('Form submitted successfully:', data);
+        // Reset form after successful submission
+        resetForm(formEl);
+      } catch (error) {
+        console.error('Error submitting the form:', error);
+      }
     } else {
-      console.log('error submit!');
+      console.log('Error submitting the form!');
     }
   });
 };
@@ -50,6 +91,8 @@ const resetForm = (formEl) => {
   if (!formEl) return;
   formEl.resetFields();
 };
+
+
 
 </script>
 
@@ -65,56 +108,63 @@ const resetForm = (formEl) => {
     label-width="auto"
     class="demo-ruleForm"
   >
-  <div>
-    <h3>Contact information</h3>
-    <!-- Firstname -->
-    <el-form-item label="Firstname" prop="firstname">
-      <el-input v-model="ruleForm.firstname" />
-    </el-form-item>
-    <!-- Lastname -->
-    <el-form-item label="Lastname" prop="lastname">
-      <el-input v-model="ruleForm.lastname" />
-    </el-form-item>
-    <!-- Email -->
-    <el-form-item label="Email" prop="email">
-      <el-input v-model="ruleForm.email" />
-    </el-form-item>
-    <!-- Phone -->
-    <el-form-item label="Phone" prop="phone">
-      <el-input v-model="ruleForm.phone" />
-    </el-form-item>
-  <!-- Password -->
-    <el-form-item label="Password" prop="pass">
-      <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
-    </el-form-item>
-    <!-- Confirm Password -->
-    <el-form-item label="Confirm password" prop="checkPass">
-      <el-input
-        v-model="ruleForm.checkPass"
-        type="password"
-        autocomplete="off"
-      />
-    </el-form-item>
+  <div id="form-fields">
+
+    <div>
+      <h3>Contact information</h3>
+      <!-- Firstname -->
+      <el-form-item label="Firstname" prop="firstname">
+        <el-input v-model="ruleForm.firstname" />
+      </el-form-item>
+      <!-- Lastname -->
+      <el-form-item label="Lastname" prop="lastname">
+        <el-input v-model="ruleForm.lastname" />
+      </el-form-item>
+      <!-- Email -->
+      <el-form-item label="Email" prop="email">
+        <el-input v-model="ruleForm.email" />
+      </el-form-item>
+      <!-- Username -->
+      <el-form-item label="Username" prop="username">
+        <el-input v-model="ruleForm.username" />
+      </el-form-item>
+      <!-- Phone -->
+      <el-form-item label="Phone" prop="phone">
+        <el-input v-model="ruleForm.phone" />
+      </el-form-item>
+    <!-- Password -->
+      <el-form-item label="Password" prop="pass">
+        <el-input v-model="ruleForm.password" type="password" autocomplete="off" />
+      </el-form-item>
+      <!-- Confirm Password -->
+      <!-- <el-form-item label="Confirm password" prop="checkPass">
+        <el-input
+          v-model="ruleForm.checkPass"
+          type="password"
+          autocomplete="off"
+        />
+      </el-form-item> -->
+    </div>
+     <div>
+       <h3>Address information</h3>
+       <el-form-item label="Street" prop="street">
+        <el-input v-model="ruleForm.address.street" />
+      </el-form-item>
+      <el-form-item label="City" prop="city">
+        <el-input v-model="ruleForm.address.city" />
+      </el-form-item>
+      <el-form-item label="Postalcode" prop="postalCode">
+        <el-input v-model="ruleForm.address.postalCode" />
+      </el-form-item>
+      <el-form-item label="Country" prop="country">
+        <el-select v-model="ruleForm.address.country" placeholder="Country">
+          <el-option label="Belgium" value="Belgium" />
+          <el-option label="Luxembourg" value="Luxembourg" />
+          <el-option label="Netherlands" value="Netherlands" />
+        </el-select>
+      </el-form-item>
+     </div>
   </div>
-   <div>
-     <h3>Address information</h3>
-     <el-form-item label="Street" prop="street">
-      <el-input v-model="ruleForm.street" />
-    </el-form-item>
-    <el-form-item label="City" prop="city">
-      <el-input v-model="ruleForm.city" />
-    </el-form-item>
-    <el-form-item label="Postalcode" prop="postalcode">
-      <el-input v-model="ruleForm.postalcode" />
-    </el-form-item>
-    <el-form-item label="Country" prop="country">
-      <el-select v-model="ruleForm.country" placeholder="Country">
-        <el-option label="Belgium" value="Belgium" />
-        <el-option label="Luxembourg" value="Luxembourg" />
-        <el-option label="Netherlands" value="Netherlands" />
-      </el-select>
-    </el-form-item>
-   </div>
 
    <el-form-item>
       <el-button type="primary" @click="submitForm(ruleFormRef)">
@@ -126,5 +176,8 @@ const resetForm = (formEl) => {
 </template>
 
 <style lang="scss" scoped>
-
+  #form-fields {
+    display: flex;
+    gap: 48px;
+  }
 </style>
